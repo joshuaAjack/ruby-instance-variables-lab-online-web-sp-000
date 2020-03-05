b18409ee7f96e4339a590ae6a4abdda9b5333bb7
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-05 02:07:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,6 @@
 class Dog 
-  
 def name=(dog_name)
-  this_dogs_name = dog_name 
+  @this_dogs_name = dog_name 
 end
 
 def name 
@@ -13,4 +12,5 @@ lassie = Dog.new
 lassie.name = "Lassie"
  
 lassie.name
+
 
